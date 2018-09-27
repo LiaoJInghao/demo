b37607675464b03c9e65c33d185d37afaf069099
Administrator <Administrator@WIN-6SBV0QG4LEF>
2018-09-27 10:41:28 +0800

    2018/9/27

---
+++ admin-dashboard/app/model/contract/ContractModel.js
@@ -0,0 +1,17 @@
+﻿Ext.define('Admin.model.contract.ContractModel', {
+	extend: 'Admin.model.Base',
+	requires: [
+		'Ext.data.proxy.Rest'
+	],
+	fields: [
+		{type:'int',name:'id'}
+		,{type:'string',name:'userId'}
+		,{type:'string',name:'contractType'}
+		,{type:'string',name:'processStatus'}
+		,{type:'string',name:'processInstanceId'}
+	],
+	proxy: {
+		type: 'rest',
+		url: '/contract',
+	}
+});
---
+++ admin-dashboard/app/model/contractapprove/ContractApproveModel.js
@@ -0,0 +1,18 @@
+﻿Ext.define('Admin.model.contractapprove.ContractApproveModel', {
+    extend: 'Admin.model.Base',
+    fields: [	//需要修改
+		 {type: 'int' ,name: 'id'}
+        ,{type: 'string' ,name: 'userId'}
+        ,{type: 'string' ,name: 'contractType'}
+        ,{type: 'string' ,name: 'processStatus'}
+        ,{type: 'string' ,name: 'processInstanceId'}
+        ,{type: 'string' ,name: 'taskId'}
+        ,{type: 'string' ,name: 'taskName'}
+        ,{type: 'date' ,name: 'taskCreateTime'}
+        ,{type: 'string' ,name: 'assignee'}
+        ,{type: 'string' ,name: 'taskDefinitionKey'}
+        ,{type: 'string' ,name: 'processDefinitionId'}
+        ,{type: 'boolean' ,name: 'suspended'}
+        ,{type: 'int' ,name: 'version'}
+    ]
+});
